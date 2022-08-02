a3b26da927be730a022d2d8d8917a481e2da9483
Joacodigo <joacobracci@gmail.com>
2022-08-02 18:12:48 -0300

    TESTING EXPRESS
    TESTING XMLHttpRequest

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var path = require("path");
 var routes = require("./routes");
+var XMLHttpRequest = require('xhr2');
 
 var app = express();
+
+
+var redirect_uri ="http://localhost:5000/home";
+var clientID = "ea854282.0bee69f4.tid_671f9079.bindid.io";
+var clientSecret = "911f24e5-5141-40b4-9ffb-115b04f48f00";
 
 
 app.set("port", process.env.PORT || 5000);
@@ -15,6 +21,24 @@ app.use(express.json());
 
 app.post("/auth", function (req,res){
     console.log(req.body.code)
+    code = req.body.code
+
+
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "https://demo.bindid-sandbox.io/demo-token-exchange", true);
+    XHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    XHR.send(JSON.stringify({
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirect_uri,
+        "client_id": clientID,
+        "client_secret": clientSecret
+    }
+    
+));
+    
+
+
     
 })
 
